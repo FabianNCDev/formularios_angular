79ad8d8c344340e9fde6fd9f5d2c35e94cfdc2f3
Fabian Jesus <fabianncdev@gmail.com>
2016-07-13 12:02:51 +0000

    formulario mas FormBuilder

--- codes/basic_sku.ts
+++ codes/basic_sku.ts
@@ -4,7 +4,7 @@
 
 import {bootstrap} from "angular2/platform/browser";
 import {Component} from "angular2/core";
-import {FORM_DIRECTIVES,FormBuilder,ControlGroup} from "angular2/common";
+import {FORM_DIRECTIVES} from "angular2/common";
 
 //sku : stockkepping unit
 @Component({
@@ -14,12 +14,12 @@ import {FORM_DIRECTIVES,FormBuilder,ControlGroup} from "angular2/common";
   <div class="ui raised segment">
     <h2 class="ui header">Formulario Stockkepping Unit:</h2>
 
-    <form [ngFormModel]="myForm" (ngSubmit)="onSubmit(myForm.value)" class="ui form">
+    <form #f="ngForm" (ngSubmit)="onSubmit(f.value)" class="ui form">
       <div class="field">
         <label for="skuInput">SKU</label>
-        <input type="text" id="skuInput" placeholder="Stockkepping Unit" [ngFormControl]="myForm.controls['sku']"/>
+        <input type="text" id="skuInput" placeholder="Stockkepping Unit" ngControl="sku"/>
       </div>
-      <button type="submit" class="ui inverted orange button">
+      <button type="submit" class="ui inverted blue button">
         Aceptar
         <i class="right arrow icon"></i>
       </button>
@@ -28,15 +28,8 @@ import {FORM_DIRECTIVES,FormBuilder,ControlGroup} from "angular2/common";
   `
 })
 class sku{
-  myForm:ControlGroup;
-
-  constructor(fb:FormBuilder){
-    this.myForm=fb.group({
-      'sku':['valor por defecto']
-    });
-  }
-  onSubmit(value:string):void{
-    console.log('has enviado el valor del formulario: ',value);
+  onSubmit(form:any):void{
+    console.log('has enviado el valor del formulario: ',form);
   }
 }
 bootstrap(sku);
